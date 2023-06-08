618cdf5b37d77eb29e634966cb1f81d8ad6821f5
Nour Hesham Shaheen <nourheshamshaheen11@gmail.com>
2023-06-08 11:59:53 +0400

    Add generate.py

---
+++ generate.py
@@ -0,0 +1,29 @@
+from vqa import VQA
+import os
+from csv import writer
+
+class Generator:
+
+    def __init__(self, model_name) -> None:
+        self.vqa_object = VQA(model_name)
+        self.vqa_object.setup()
+
+    def generate_all(self, image_folder, questions, output_file) -> None:
+        image_paths = os.listdir(image_folder)
+        for image_path in image_paths:
+            self.generate(image_path=image_path, questions=questions, output_file=output_file)
+            
+    def generate(self, image_path, questions, output_file):
+        answers = []
+        for question in questions:
+            answer = self.vqa_object.answer(image_path, question)
+            answers.append(answer)
+        self.append(image_path, answers, output_file)
+
+    def append(self, image_path, answers, output_file) -> None:
+        with open(output_file, 'a') as f_object:
+            description = " ".join(answers)
+            row = [image_path, description]
+            writer_object = writer(f_object)
+            writer_object.writerow(row)
+            f_object.close()
